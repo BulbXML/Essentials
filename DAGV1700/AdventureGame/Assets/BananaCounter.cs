c3598e49233ecdc664e3dfa561774f2fb2501978
BulbXML <baileytibbles2@gmail.com>
2025-10-24 20:16:28 -0600

    uniy 8 or 9 what are we on

--- DAGV1700/AdventureGame/Assets/BananaCounter.cs
+++ DAGV1700/AdventureGame/Assets/BananaCounter.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
+using TMPro;
 
 public class BananaCounter : MonoBehaviour
 {
     public int bananas = 0;           // Current banana count
     public GameObject Chest;          // Chest to activate
     public int triggerAmount = 7;     // Number of bananas to open the chest
+    public TextMeshProUGUI bananaText; // UI text to display banana count
 
     // Call this to increase the banana count
+    void Start()
+    {
+        // Initialize chest as inactive
+        Chest.SetActive(false);
+        UpdateBananaText();
+    }
     public void AddBanana(int amount = 1)
     {
         bananas += amount;
         Debug.Log("Bananas: " + bananas);
+        UpdateBananaText();
 
         // Activate the chest when threshold is reached
         if (bananas >= triggerAmount && !Chest.activeSelf)
         {
             Chest.SetActive(true);
             Debug.Log("Chest is now active!");
+        }
+    }
+    void UpdateBananaText()
+    {
+        if (bananaText != null)
+        {
+            bananaText.text = bananas.ToString();
         }
     }
 }
